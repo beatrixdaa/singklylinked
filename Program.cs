43bceee7ddfd0fb721c2bffb9e339dccf53302f6
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-11-11 17:16:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Singly_Linked_List
+
+{
+    //each node_consist of the information part and lik to the next mode
+
+    class Node
+    {
+        public int rolNumber;
+        public string name;
+        public Node next;
+    }
+    class List
+    {
+        Node START;
+
+        public List()
+        {
+            START = null;
+        }
+       
+
+    }
+}
